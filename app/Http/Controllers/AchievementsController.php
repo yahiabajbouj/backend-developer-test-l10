f5d125c6a05fe93ba808206b1e35b035972ca6df
yahia bajbouj <yahia.bajbouj@gmail.com>
2023-12-23 14:49:59 +0300

    Achievements Endpoint

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -2,19 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\UserService;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AchievementsController extends Controller
 {
+    public $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     public function index(User $user)
     {
+        $achievementsCount = $this->userService->getAchievementsCount($user);
+        $badgeName = $this->userService->getCurrentBadgeName($achievementsCount);
+        $nextBadge = $this->userService->getNextBadge($achievementsCount);
+
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $this->userService->getUnlockedAchievements($user),
+            'next_available_achievements' => $this->userService->getNextAvailableAchievements($user),
+            'current_badge' => $badgeName,
+            'next_badge' => $nextBadge->name,
+            'remaing_to_unlock_next_badge' => $nextBadge->achievements_count_needed - $achievementsCount
         ]);
     }
 }
--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -22,4 +22,44 @@ class UserService
     {
         return Badge::where('achievements_count_needed', '<=', $achievementsCount)->orderBy('id', 'desc')->first()->name;
     }
+
+    public function getNextBadge($achievementsCount)
+    {
+        $badge = Badge::where('achievements_count_needed', '>', $achievementsCount)->orderBy('id', 'asc')->first();
+        if(!isset($badge)) $badge = Badge::orderBy('id', 'desc')->first();
+        return $badge;
+    }
+
+    public function getUnlockedAchievements(User $user)
+    {
+        $unlockAchievements = [];
+        $achievements = Achievement::where(function ($query) use ($user) {
+            $query->where('type', AchievementTypeDefinition::COMMENT)->where('point', '<=', $user->comments()->count());
+        })->orWhere(function ($query) use ($user) {
+            $query->where('type', AchievementTypeDefinition::LESSON)->where('point', '<=', $user->watched()->count());
+        })->get();
+
+        foreach ($achievements as $achievement) {
+            $unlockAchievements[] = $achievement->name;
+        }
+
+        return $unlockAchievements;
+    }
+
+    public function getNextAvailableAchievements(User $user)
+    {
+        $nextAvailableAchievements = [];
+
+        $achievement = Achievement::where('type', AchievementTypeDefinition::COMMENT)->where('point', '>', $user->comments()->count())->orderBy('id', 'asc')->first();
+        if($achievement){
+            $nextAvailableAchievements[] = $achievement->name;
+        }
+
+        $achievement = Achievement::where('type', AchievementTypeDefinition::LESSON)->where('point', '>', $user->watched()->count())->orderBy('id', 'asc')->first();
+        if($achievement){
+            $nextAvailableAchievements[] = $achievement->name;
+        }
+      
+        return $nextAvailableAchievements;
+    }
 }
